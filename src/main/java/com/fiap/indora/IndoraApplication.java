7a5501c95eaa4e790fd461db9813f9fc39ea3864
Yan Velasco <yancarlosduartevelasco@gmail.com>
2025-06-15 14:48:37 -0300

    initialize project structure with Spring Boot, PostgreSQL, and Maven wrapper

---
+++ src/main/java/com/fiap/indora/IndoraApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.indora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiap/indora/IndoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.indora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
